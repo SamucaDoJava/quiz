5502280808b004bda9259422f1a362758f461766
marciata <applications.bitwos@gmail.com>
2024-02-12 16:47:19 -0300

    incluindo liberacao de cors para frontend

--- src/main/java/com/game/maker/QuizApplication.java
+++ src/main/java/com/game/maker/QuizApplication.java
@@ -2,12 +2,29 @@ package com.game.maker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class QuizApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200", "http://maisumaurlqualquer")
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+//						.allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+//						.allowCredentials(true);
+			}
+		};
 	}
 
 }
