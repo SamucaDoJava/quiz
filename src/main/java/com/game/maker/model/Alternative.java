d57bfa1882c0f22a745be90fdd5b9328a29b8862
Djair Alves <djair.dj@gmail.com>
2024-02-20 15:01:48 -0300

    Formatação e otimização nos construtores

--- src/main/java/com/game/maker/model/Alternative.java
+++ src/main/java/com/game/maker/model/Alternative.java
@@ -7,140 +7,130 @@ import jakarta.persistence.*;
 @Table(name = "tb_alternativa")
 public class Alternative {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tb_alternativa_seq")
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tb_alternativa_seq")
+  private Long id;
 
-    @Column(name = "alternativa")
-    private String alternative;
+  @Column(name = "alternativa")
+  private String alternative;
 
-    @Column(name = "correta")
-    private Boolean itsCorrect;
+  @Column(name = "correta")
+  private Boolean itsCorrect;
 
-    @Column(name = "letra-referencia")
-    private String referenceLetter;
+  @Column(name = "letra-referencia")
+  private String referenceLetter;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "id_questao")
-    private Question question;
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "id_questao")
+  private Question question;
 
+  public Alternative() {
+  }
 
-    public Alternative(){
+  public Alternative(String referenceLetter, String alternative, Boolean itsCorrect) {
+    this.alternative = alternative;
+    this.itsCorrect = itsCorrect;
+    this.referenceLetter = referenceLetter;
+  }
 
+  public Alternative(Long id, String referenceLetter, String alternative, Boolean itsCorrect) {
+    this(referenceLetter, alternative, itsCorrect);
+    this.id = id;
+  }
+
+  public Alternative(Long id, String alternative, Boolean itsCorrect, String referenceLetter, Question question) {
+    this(id, referenceLetter, alternative, itsCorrect);
+    this.question = question;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getAlternative() {
+    return alternative;
+  }
+
+  public void setAlternative(String alternative) {
+    this.alternative = alternative;
+  }
+
+  public Boolean getItsCorrect() {
+    return itsCorrect;
+  }
+
+  public void setItsCorrect(Boolean correct) {
+    itsCorrect = correct;
+  }
+
+  public String getReferenceLetter() {
+    return referenceLetter;
+  }
+
+  public void setReferenceLetter(String referenceLetter) {
+    this.referenceLetter = referenceLetter;
+  }
+
+  public Question getQuestion() {
+    return question;
+  }
+
+  public void setQuestion(Question question) {
+    this.question = question;
+  }
+
+  @Override
+  public String toString() {
+    return "Alternative: " +
+        "---> id= " + id +
+        "---> ReferenceLetter= " + referenceLetter +
+        "---> Alternative= " + alternative;
+  }
+
+  public static final class Builder {
+    private Alternative alternativeBuilder;
+
+    private Builder() {
+      alternativeBuilder = new Alternative();
     }
 
-    public Alternative(String referenceLetter, String alternative, Boolean itsCorrect) {
-        this.alternative = alternative;
-        this.itsCorrect = itsCorrect;
-        this.referenceLetter = referenceLetter;
+    public static Builder anAlternative() {
+      return new Builder();
     }
 
-    public Alternative(Long id, String reference, String alternative, Boolean itsCorrect) {
-        this.id = id;
-        this.alternative = alternative;
-        this.itsCorrect = itsCorrect;
-        this.referenceLetter = referenceLetter;
+    public Builder id(Long id) {
+      alternativeBuilder.setId(id);
+      return this;
     }
 
-    public Alternative(Long id, String alternative, Boolean itsCorrect, String referenceLetter, Question question) {
-        this.id = id;
-        this.alternative = alternative;
-        this.itsCorrect = itsCorrect;
-        this.referenceLetter = referenceLetter;
-        this.question = question;
+    public Builder alternative(String alternative) {
+      alternativeBuilder.setAlternative(alternative);
+      return this;
     }
 
-    public Long getId() {
-        return id;
+    public Builder itsCorrect(Boolean itsCorrect) {
+      alternativeBuilder.setItsCorrect(itsCorrect);
+      return this;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public Builder reference(String referenceLetter) {
+      alternativeBuilder.setReferenceLetter(referenceLetter);
+      return this;
     }
 
-
-    public String getAlternative() {
-        return alternative;
+    public Builder question(Question question) {
+      alternativeBuilder.setQuestion(question);
+      return this;
     }
 
-    public void setAlternative(String alternative) {
-        this.alternative = alternative;
+    public Alternative build() {
+      return alternativeBuilder;
     }
 
+  }
 
-    public Boolean getItsCorrect() {
-        return itsCorrect;
-    }
-
-    public void setItsCorrect(Boolean correct) {
-        itsCorrect = correct;
-    }
-
-
-    public String getReferenceLetter() {
-        return referenceLetter;
-    }
-
-    public void setReferenceLetter(String referenceLetter) {
-        this.referenceLetter = referenceLetter;
-    }
-
-    public Question getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(Question question) {
-        this.question = question;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Alternative: " +
-                "---> id= " + id +
-                "---> ReferenceLetter= " + referenceLetter +
-                "---> Alternative= " + alternative;
-    }
-
-
-    public static final class Builder {
-        private Alternative alternativeBuilder;
-
-        private Builder() {
-            alternativeBuilder = new Alternative();
-        }
-
-        public static Builder anAlternative() {
-            return new Builder();
-        }
-
-        public Builder id(Long id) {
-            alternativeBuilder.setId(id);
-            return this;
-        }
-
-        public Builder alternative(String alternative) {
-            alternativeBuilder.setAlternative(alternative);
-            return this;
-        }
-
-        public Builder itsCorrect(Boolean itsCorrect) {
-            alternativeBuilder.setItsCorrect(itsCorrect);
-            return this;
-        }
-
-        public Builder reference(String referenceLetter) {
-            alternativeBuilder.setReferenceLetter(referenceLetter);
-            return this;
-        }
-
-        public Builder question(Question question) {
-            alternativeBuilder.setQuestion(question);
-            return this;
-        }
-
-        public Alternative build() {
-            return alternativeBuilder;
-        }
-    }
 }
--- src/main/java/com/game/maker/model/GameConfiguration.java
+++ src/main/java/com/game/maker/model/GameConfiguration.java
@@ -1,6 +1,5 @@
 package com.game.maker.model;
 
-
 import jakarta.persistence.*;
 
 @SequenceGenerator(name = "tb_configuracao_jogo_seq", allocationSize = 1)
@@ -8,106 +7,106 @@ import jakarta.persistence.*;
 @Table(name = "tb_configuracao_jogo")
 public class GameConfiguration {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tb_configuracao_jogo_seq")
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tb_configuracao_jogo_seq")
+  private Long id;
 
-    @Column(name = "pontuacao_por_acerto")
-    private Long scorePerHit;
+  @Column(name = "pontuacao_por_acerto")
+  private Long scorePerHit;
 
-    @Column(name = "moeda_inicial")
-    private Long initialCoins;
+  @Column(name = "moeda_inicial")
+  private Long initialCoins;
 
-    @Column(name = "level")
-    private String level;
+  @Column(name = "level")
+  private String level;
 
+  public GameConfiguration() {
+  }
 
-    public GameConfiguration() {
+  public GameConfiguration(Long id, Long scorePerHit, Long initialCoins, String level) {
+    this.id = id;
+    this.scorePerHit = scorePerHit;
+    this.initialCoins = initialCoins;
+    this.level = level;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getScorePerHit() {
+    return scorePerHit;
+  }
+
+  public void setScorePerHit(Long scorePerHit) {
+    this.scorePerHit = scorePerHit;
+  }
+
+  public Long getInitialCoins() {
+    return initialCoins;
+  }
+
+  public void setInitialCoins(Long initialCoins) {
+    this.initialCoins = initialCoins;
+  }
+
+  public String getLevel() {
+    return level;
+  }
+
+  public void setLevel(String level) {
+    this.level = level;
+  }
+
+  @Override
+  public String toString() {
+    return "GameConfiguration{" +
+        "id=" + id +
+        ", scorePerHit=" + scorePerHit +
+        ", initialCoins=" + initialCoins +
+        ", level='" + level + '\'' +
+        '}';
+  }
+
+  public static final class Builder {
+    private GameConfiguration gameConfiguration;
+
+    private Builder() {
+      gameConfiguration = new GameConfiguration();
     }
 
-    public GameConfiguration(Long id, Long scorePerHit, Long initialCoins, String level) {
-        this.id = id;
-        this.scorePerHit = scorePerHit;
-        this.initialCoins = initialCoins;
-        this.level = level;
+    public static Builder aGameConfiguration() {
+      return new Builder();
     }
 
-    public Long getId() {
-        return id;
+    public Builder id(Long id) {
+      gameConfiguration.setId(id);
+      return this;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public Builder scorePerHit(Long scorePerHit) {
+      gameConfiguration.setScorePerHit(scorePerHit);
+      return this;
     }
 
-    public Long getScorePerHit() {
-        return scorePerHit;
+    public Builder initialCoins(Long initialCoins) {
+      gameConfiguration.setInitialCoins(initialCoins);
+      return this;
     }
 
-    public void setScorePerHit(Long scorePerHit) {
-        this.scorePerHit = scorePerHit;
+    public Builder level(String level) {
+      gameConfiguration.setLevel(level);
+      return this;
     }
 
-    public Long getInitialCoins() {
-        return initialCoins;
+    public GameConfiguration build() {
+      return gameConfiguration;
     }
 
-    public void setInitialCoins(Long initialCoins) {
-        this.initialCoins = initialCoins;
-    }
-
-    public String getLevel() {
-        return level;
-    }
-
-    public void setLevel(String level) {
-        this.level = level;
-    }
-
-    @Override
-    public String toString() {
-        return "GameConfiguration{" +
-                "id=" + id +
-                ", scorePerHit=" + scorePerHit +
-                ", initialCoins=" + initialCoins +
-                ", level='" + level + '\'' +
-                '}';
-    }
-
-    public static final class Builder {
-        private GameConfiguration gameConfiguration;
-
-        private Builder() {
-            gameConfiguration = new GameConfiguration();
-        }
-
-        public static Builder aGameConfiguration() {
-            return new Builder();
-        }
-
-        public Builder id(Long id) {
-            gameConfiguration.setId(id);
-            return this;
-        }
-
-        public Builder scorePerHit(Long scorePerHit) {
-            gameConfiguration.setScorePerHit(scorePerHit);
-            return this;
-        }
-
-        public Builder initialCoins(Long initialCoins) {
-            gameConfiguration.setInitialCoins(initialCoins);
-            return this;
-        }
-
-        public Builder level(String level) {
-            gameConfiguration.setLevel(level);
-            return this;
-        }
-
-        public GameConfiguration build() {
-            return gameConfiguration;
-        }
-    }
+  }
 
 }
